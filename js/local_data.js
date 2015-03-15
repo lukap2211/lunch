2a5ad66c8a79005dcb49dd78ae71d64ab642d944
lukap <lukap2211@gmail.com>
2015-03-15 01:30:24 -0400

    Adding local data

---
+++ js/local_data.js
@@ -0,0 +1,56 @@
+var LOCAL = {
+
+  user : {
+  "username": "lukap",
+  "first_name": "Luka",
+  "last_name": "Puharic",
+  "display_name": "Luka Puharic",
+  "is_friend": false,
+  "friends_count": 3,
+  "about": "legal alien in NY",
+  "email": null,
+  "phone": null,
+  "profile_picture_url": "https://venmopics.appspot.com/u/v6/f/e5938118-4cb7-4f2c-9319-866d8c0d2d36",
+  "friend_request": {
+    "status": null
+  },
+  "trust_request": {
+    "status": null
+  },
+  "id": "1607579096580096437",
+  "date_joined": "2015-01-28T00:52:42"
+  },
+
+  friends : [
+    {
+      "username": "sungok",
+      "first_name": "Sungok",
+      "last_name": "Lim",
+      "display_name": "Sungok Lim",
+      "about": "No Short Bio",
+      "profile_picture_url": "https://venmopics.appspot.com/u/v1/s/99cf4980-2bd2-4c5e-a2d0-e85630dba716",
+      "id": "1614041571131392009",
+      "date_joined": "2015-02-05T22:52:29"
+    },
+    {
+      "username": "Marcin-Staszewski",
+      "first_name": "Marcin",
+      "last_name": "Staszewski",
+      "display_name": "Marcin Staszewski",
+      "about": "No Short Bio",
+      "profile_picture_url": "https://venmopics.appspot.com/u/v2/s/5c21152c-ddf4-4990-a09d-61dd41e9ca92",
+      "id": "1614000936714240984",
+      "date_joined": "2015-02-05T21:31:45"
+    },
+    {
+      "username": "Pawel-Raszewski",
+      "first_name": "Pawel",
+      "last_name": "Raszewski",
+      "display_name": "Pawel Raszewski",
+      "about": "No Short Bio",
+      "profile_picture_url": "https://venmopics.appspot.com/u/v1/s/ba0f2150-c264-4634-92d1-53802e662914",
+      "id": "1612520473231361012",
+      "date_joined": "2015-02-03T20:30:20"
+    }
+  ]
+}
